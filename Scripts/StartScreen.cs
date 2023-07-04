0049347571d4677418bc19bdbe40191514096f8d
nix0nax <46956959+nix0nax@users.noreply.github.com>
2023-07-04 23:41:31 +0200

    Start screen (not done)

---
+++ Scripts/StartScreen.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+public partial class StartScreen : Control
+{
+	AnimationPlayer animationPlayer;
+	bool aboutToStart;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		animationPlayer = this.GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Play("TextBob");
+
+		aboutToStart = false;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Light"))
+		{
+			animationPlayer.Play("Start");
+			aboutToStart = true;
+			//this.GetTree().ChangeSceneToFile("res://Scenes/main_scene.tscn");
+		}
+
+		if (Input.IsActionJustPressed("Escape"))
+		{
+			this.GetTree().Quit();
+		}
+	}
+
+	public void AnimationDone(string animName)
+	{
+		Trace.Write("animationdone");
+		if (aboutToStart)
+		{
+			this.GetTree().ChangeSceneToFile("res://Scenes/main_scene.tscn");
+		}
+	}
+}
